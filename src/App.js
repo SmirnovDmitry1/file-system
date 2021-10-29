eef0866cd5f627444be69dd73976acfcc171ffce
SmirnovDmitry1 <smirnovdmitry322@gmail.com>
2021-10-29 09:39:56 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [ textFile, setTextFile ] = useState('')
+  const [ globalFileHandle, setGlobalFileHandle] = useState([])
+
+  const openFile = async () => {
+    let [fileHandle] = await window.showOpenFilePicker()
+    let fileData = await fileHandle.getFile()
+    let text = await fileData.text();
+    setGlobalFileHandle(fileHandle)
+    setTextFile(text)
+  }
+
+  const saveFile = async () => {
+    let stream = await globalFileHandle.createWritable()
+    await stream.write(document.getElementById('textarea').innerText)
+    await stream.close()
+    setTextFile('')
+  }
+
+  const saveAs = async () => {
+    let fileHandle = await window.showSaveFilePicker()
+    let stream = await fileHandle.createWritable()
+    await stream.write(document.getElementById('textarea').innerText)
+    await stream.close()
+    setTextFile('')
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => openFile()} >Open file</button>
+        <pre id='textarea' contentEditable>{textFile}</pre>
+        <button onClick={() => saveFile()} >Save file</button>
+        <button onClick={( ) => saveAs()} >Save As</button>
       </header>
     </div>
   );
